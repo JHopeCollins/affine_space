bd0d14c73db6622e8dcbbbd7a47b0b63b62fcc84
JHopeCollins <jhc.jss@gmail.com>
2022-01-09 14:36:57 +0000

    added license text to affine_space.h

--- affine_space/affine_space.h
+++ affine_space/affine_space.h
@@ -1,11 +1,33 @@
 
 # pragma once
 
-# include <array>
-# include <concepts>
-# include <type_traits>
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2022 Joshua T. Hope-Collins
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 /*
+ * https://github.com/JHopeCollins/affine_space
+ *
  * This header defines two base types corresponding to a point and a displacement (delta) in an affine space.
  *
  *    These types must both be inherited from using CRTP. Instances of the two derived types will act as members of an affine space, with only the expected arithmetic operations allowed,
@@ -64,6 +86,10 @@
  *       // x0 =  a*x1;    // will not compile!
  *
  */
+
+# include <array>
+# include <concepts>
+# include <type_traits>
 
 namespace affine
 {
